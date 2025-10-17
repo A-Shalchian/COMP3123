14538774d357b7eca6e1cc2fff96755a4bcfe112
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-10-17 00:15:28 -0400

    labex7 done

---
+++ exec07/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week07 or 09 Lab exercise</h2>
+        <p className="info">101414035</p>
+        <p className="info">Arash Shalchian</p>
+        <p className="school">George Brown College, Toronto</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
